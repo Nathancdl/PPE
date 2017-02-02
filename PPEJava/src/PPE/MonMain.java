e88eccc26130e58a57be40b6c2fdd1fcc0b91666
Nathan <nathan.caudeli@hotmail.com>
2017-02-02 22:32:56 +0100

    pate

--- PPEJava/src/PPE/MonMain.java
+++ PPEJava/src/PPE/MonMain.java
@@ -1,5 +1,5 @@
 package PPE;
-
+import java.util.Scanner;
 import database.*;
 import inscriptions.*;
 import Presentation.*;
@@ -7,11 +7,31 @@ public class MonMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		MainMenu mainMenu = new MainMenu(Inscriptions.getInscriptions());
+		Scanner sc = new Scanner(System.in);
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		System.out.println("Nouvel compet :");
+		String compet = sc.nextLine();
+		Competition taist = inscriptions.createCompetition(compet, null, false);
+		
+		System.out.println("Nouvel equipe :");
+		String equip = sc.nextLine();
+		Equipe taiist = inscriptions.createEquipe(equip);
 		
 		
-
+		System.out.println("user :");
+		String nom = sc.nextLine();
+		System.out.println("equipe :");
+		String equipe = sc.nextLine();
+		System.out.println("mdp :");
+		String mdp = sc.nextLine();
+		
+		Personne test = inscriptions.createPersonne(nom, equipe, mdp); 
+		taist.add(test);
+		taiist.add(test);
+		
+		System.out.println(test);
+		
+		
 	}
 
 }
