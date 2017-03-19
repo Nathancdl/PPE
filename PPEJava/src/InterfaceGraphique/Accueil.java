4fc0add68a85f4e578489fc29805ff1afd1831d0
Nathan <nathan.caudeli@hotmail.com>
2017-03-19 16:55:00 +0100

    Interface graphique start

---
+++ PPEJava/src/InterfaceGraphique/Accueil.java
@@ -0,0 +1,28 @@
+package InterfaceGraphique;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Accueil {
+	
+	public Accueil(){
+		fenetre = new JFrame("NTM");
+		fenetre.setBounds(300, 300, 410, 150);
+		
+		pano = new JPanel();
+		pano.setLayout(new GridLayout(1,1));
+		fenetre.add(pano);
+		fenetre.setVisible( true );
+	}
+	
+	public static void main(String[] args)
+	{
+		new Accueil();
+		new AfficheUser();
+	}
+	
+	JFrame fenetre;
+	
+	JPanel pano;
+
+}
---
+++ PPEJava/src/InterfaceGraphique/AfficheUser.java
@@ -0,0 +1,61 @@
+package InterfaceGraphique;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class AfficheUser extends JFrame implements ActionListener {
+	private String mot;
+	private JButton affiche, efface;
+	private JLabel affichage;
+	private JPanel pano;
+	
+	public AfficheUser(){
+		setSize(300, 100);
+		setTitle("Boutons");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pano = new JPanel(new BorderLayout(40,50));
+		affiche = new JButton("affiché");
+		efface = new JButton("Supprime");
+		affiche.addActionListener(this);
+		efface.addActionListener(this);
+		
+		affichage = new JLabel();
+		pano.add(efface, BorderLayout.EAST);
+		pano.add(affiche, BorderLayout.WEST);
+		
+		add(pano);
+		setVisible(true);
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if ( e.getSource() == affiche )
+		{
+			pano.remove(affichage);
+			mot = "Bonjour";
+			affichage = new JLabel(mot);
+			 
+			pano.add(affichage, BorderLayout.CENTER);
+			pano.repaint();
+			pano.validate();
+			
+			
+			
+		}
+		else if( e.getSource() == efface )
+		{
+			pano.remove(affichage);
+			pano.repaint();
+			mot = "";
+			pano.validate();
+		}
+			
+	}
+
+	
+
+}
