82aff8fbf9b3a73dcb68a68697ee68cc7453be89
Nathan <nathan.caudeli@hotmail.com>
2017-04-21 14:48:41 +0200

    Fix

--- PPEJava/src/InterfaceGraphique/PanneauTest.java
+++ PPEJava/src/InterfaceGraphique/PanneauTest.java
@@ -5,36 +5,38 @@ import net.proteanit.sql.DbUtils;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 
-import com.mysql.jdbc.PreparedStatement;
+
 
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.awt.event.ActionEvent;
-import java.io.Serializable;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
+
 
 public class PanneauTest extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
 	private JTable table;
 	Connect bdd = new Connect();
-	/**
-	 * Create the panel.
-	 */
+	private JPanel panelTest = new JPanel();
+	
 	public PanneauTest() {
-		
+		JButton btnLoadTable = new JButton("Load user");
+		add(btnLoadTable);
 		JScrollPane scrollPane = new JScrollPane();
 		add(scrollPane);
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
 		
-		JButton btnLoadTable = new JButton("Load user");
+		
 		btnLoadTable.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
@@ -57,7 +59,7 @@ public class PanneauTest extends JPanel {
 				
 			}
 		});
-		add(btnLoadTable);
+		
 
 	}
 
