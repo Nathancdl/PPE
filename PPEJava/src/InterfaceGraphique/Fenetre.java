eb6c3ace5685a81eb860c4206ba95e80c31ab0c6
Nathan <nathan.caudeli@hotmail.com>
2017-04-21 16:07:17 +0200

    Fenetre clean

--- PPEJava/src/InterfaceGraphique/Fenetre.java
+++ PPEJava/src/InterfaceGraphique/Fenetre.java
@@ -23,10 +23,10 @@ public class Fenetre extends JFrame
 
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu menuFichier = new JMenu("Fichier");
-	private JMenu menuEdition = new JMenu("Edition");
+	
 	private JMenu menuAide = new JMenu("Aide");
 	
-	private JMenuItem item1 = new JMenuItem("Ouvrir");
+	
 	private JMenuItem item2 = new JMenuItem("Fermer");
 	private JMenuItem item3 = new JMenuItem("Lancer");
 	private JMenuItem item4 = new JMenuItem("Arrêter");
@@ -47,26 +47,18 @@ public class Fenetre extends JFrame
 	    this.setLocationRelativeTo(null);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setContentPane(panneau);
-	    menuFichier.add(item1);
+	   
 		menuFichier.addSeparator();
 	    menuFichier.add(item2);
-		menuEdition.add(item3);
-		menuEdition.addSeparator();
-		menuEdition.add(item4);
+		
 		menuAide.add(item5);
 		menuAide.addSeparator();
 		menuAide.add(item6);
 		menuBar.add(menuFichier);
-		menuBar.add(menuEdition);
+		
 		menuBar.add(menuAide);
 		
-		item1.addActionListener( new ActionListener(){
-			public void actionPerformed( ActionEvent e )
-			{
-				dispose();
-				
-			}
-		});
+		
 		item2.addActionListener( new ActionListener(){
 			public void actionPerformed( ActionEvent e )
 			{
